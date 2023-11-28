4eae3630cb0606f08d6243418e740dacdf109f38
Alfian <alfianmusthofa4564@gmail.com>
2023-11-28 15:34:04 +0700

    cont

--- app/views/todo/index.php
+++ app/views/todo/index.php
@@ -8,7 +8,7 @@
     </div>
 </section>
 <div class="hero">
-    <img src="https://placehold.co/2000x400" alt="">
+    <img src="<?= BASEURL ?>/img/todo/bg.webp" alt="">
     <div class="content flex flex-col gap-2 text-center">
         <h3 class="text-2xl font-semibold text-white">Ciptakan momen tak terlupakan dari sini</h3>
         <div class="input-field">
@@ -16,3 +16,66 @@
         </div>
     </div>
 </div>
+<div class="menus grid grid-cols-9 place-items-center">
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (1).png" alt="">
+        <p>Wifi</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (2).png" alt="">
+        <p>Olahraga</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (3).png" alt="">
+        <p>Transportasi</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (4).png" alt="">
+        <p>Tur</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (5).png" alt="">
+        <p>Kecantikan</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (6).png" alt="">
+        <p>Hiburan</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (7).png" alt="">
+        <p>Bermain</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (8).png" alt="">
+        <p>Event</p>
+    </div>
+    <div class="col">
+        <img src="<?= BASEURL ?>/img/icon/todo (9).png" alt="">
+        <p>JR Pass</p>
+    </div>
+</div>
+<section class="container">
+    <div class="title">
+        <h3 class="text-xl font-semibold">Saatnya Bertualang</h3>
+        <p class="text-sm mt-1">Cek rekomendasi aktivitas keren diberbagai negara, kuy!</p>
+    </div>
+    <div class="row mt-4">
+        <div class="box">
+            <a href="#">
+                <img src="<?= BASEURL ?>/img/todo/card (1).png" alt="">
+                <div class="caption">
+                    <h4 class="title">Universal Studio</h4>
+                    <div class="address">
+                        <p>Singapura</p>
+                    </div>
+                    <div class="review">
+                        <p>4/5 (1.200 review)</p>
+                    </div>
+                    <div class="price">
+                        <h4>IDR 350.000</h4>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
